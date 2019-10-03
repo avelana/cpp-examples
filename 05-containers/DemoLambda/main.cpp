10cafcb4e6569dbff03a420ed04202c09f358911
avelana <o.g.kazantseva@gmail.com>
2019-10-03 13:31:24 +0300

    count_if, lambda

--- 05-containers/DemoLambda/main.cpp
+++ 05-containers/DemoLambda/main.cpp
@@ -2,11 +2,10 @@
 #include <vector>
 #include <algorithm>
 
-int &Min(int &a, int &b) {
-  if (a < b) {
-    return a;
-  }
-  return b;
+// Возврат по ссылке обычно используется для возврата аргументов, переданных в функцию по ссылке.
+// Здесь допустим возврат по ссылке
+int& Min(int &a, int &b) {
+  return (a < b)? a :b;
 }
 
 int &Max(int &a, int &b) {
@@ -30,6 +29,29 @@ void PrintContainer(std::vector<int> &vec, const std::string &title) {
   std::cout << '\n';
 }
 
+// Возврат по ссылке обычно используется для возврата аргументов, переданных в функцию по ссылке.
+// Здесь НЕ допустим возврат по ссылке, только по значению
+// ! НЕЛЬЗЯ возвращать ссылку на локальныю переменную, которая уничтожается после выполнения функции.
+// Подсчет количества вхождений значения value
+template<typename T>
+// int& CountItem(std::vector<T> vec, T value) {  // ERROR int&
+int CountItem(std::vector<T> vec, T value) {
+  int count = 0;
+  for (auto i : vec) {
+    if (i == value) {
+      ++count;
+    }
+  }
+  return count;
+}
+// Greater than 2
+bool Gt2(int x) {
+  return (x > 2) ? true : false;
+}
+// Less than 2
+bool Lt2(int x) {
+  return (x < 2) ? true : false;
+}
 int main() {
   int a = 2, b = 3;
   std::cout << "our function Min(" << a << ", " << b << ") = " << Min(a, b) << std::endl; // 2
@@ -50,6 +72,7 @@ int main() {
   std::cout << "STL function max(" << s1 << ", " << s2 << ") = " << std::max(s1, s2) << std::endl;
 
   std::vector<int> vec = {4, 2, 3, 6, 3, 4, 5, 2, 1, 9};
+ // std::cout << "Min element in the vector = " << std::min(vec) << '\n';
   std::cout << "Min element in the vector = " << *(std::min_element(vec.begin(), vec.end())) << '\n';
 
 /*
@@ -61,7 +84,7 @@ int main() {
   std::cout << "Initial vector: ";
   PrintContainer(vec);
 
-  // std::sort(vec.begin(), vec.end());  // 1 способ
+  std::sort(vec.begin(), vec.end());  // 1 способ
   std::sort(std::begin(vec), std::end(vec)); // 2 способ
   PrintContainer(vec, "Sorted vector");
 
@@ -77,15 +100,60 @@ int main() {
   for (auto it = numbers.cbegin(); it != numbers.cend(); it++) {
     sum += *it; // разыменование итератора
   }
+
+  // std::accumulate
+
   std::cout << "Sum numbers:" << sum << '\n';
 
-  // Печатает первое число в векторе numbers
-  // и первый фрукт в векторе fruit, не проверяя, есть ли он.
+  // Печатает значение первого элемента в векторе numbers и fruit,
+  // не проверяя, есть ли он.
   std::cout << "The first number: " << *numbers.begin() << '\n';
   std::cout << "The first fruit: " << *fruits.begin() << '\n';
 
+  // Печатает значение последнего элемента в векторе numbers и fruit,
+  // не проверяя, есть ли он.
+  std::cout << "The last number: " << *(numbers.end()-1) << '\n';
+  std::cout << "The last fruit: " << *(fruits.end()-1) << '\n';
+
   if (empty.begin() == empty.end())
     std::cout << "Vector 'empty' is really empty.\n";
+
+  // Печатает значение первого и последнего  элемента в векторе numbers.
+  // Обращение с помощью front(), back()
+  std::cout << "The first number (front): " << numbers.front() << '\n';
+  std::cout << "The last number (back): " << numbers.back() << '\n';
+
+  std::vector<char> letters{'o', 'm', 'g', 'w', 't', 'o', 'f'};
+
+  if (!letters.empty()) {
+    std::cout << "The first character is: " << letters.front() << '\n';
+    std::cout << "The last character is: " << letters.back() << '\n';
+  }
+
+  // Подсчет количества вхождений значения 'o'
+  int cnt = CountItem(letters, 'o');
+  std::cout << "The number of the letter 'o' is equal " << cnt << '\n';
+
+  // Подсчет количества вхождений значения 'o' c с использованием std::count
+  std::cout << "The number of the letter 'o' is equal " << std::count(begin(letters), end(letters), 'o') << '\n';
+
+  // count_if
+  std::vector<int> v = {1, 3, 2, 5, 4, 8, 1, 9};
+  std::cout << "The number of items are greater than 2 = " << count_if(begin(v), end(v), Gt2) << '\n';
+  std::cout << "The number of items are less than 2 = " << count_if(begin(v), end(v), Lt2) << '\n';
+
+  // C Лямбда
+  std::cout << count_if(begin(v), end(v), [](auto& x) { return (x > 2) ? true : false; }) << '\n';
+
+  int n = 5;
+  std::cout << "Greater than " << n << " = "<<count_if(begin(v), end(v), [n](auto& x) { return (x > n) ? true : false; });
+
+  int data[] = { 1, 2, 3, 4, 4, 3, 7, 8, 9, 10 };
+  std::vector<int> ve(data, data+10);
+
+  int num_items1 = std::count_if(begin(ve), end(ve),
+      [](int& x) {return x % 3 == 0;});
+  std::cout << "The number of numbers divisible by three: " << num_items1 << '\n';
 
   return 0;
 }
