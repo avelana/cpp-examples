00f065eda5dff612e228f238eed31401caf0fa94
Volha Kazantsava <avelana@users.noreply.github.com>
2019-10-06 18:53:16 +0300

    example of different declaration of variables of the user data type

--- 07-structures-classes/DemoStructure/main.cpp
+++ 07-structures-classes/DemoStructure/main.cpp
@@ -2,30 +2,61 @@
 #include <vector>
 using namespace std;
 
-void PrintClass(const string &title, const string &date, const string &time, unsigned short &room) {
-  cout << "Title: " << title
-       << ", date: " << date
-       << ", time: " << time
-       << ", room: " << room << '\n';
+struct Lesson {
+  string title;
+  string date;
+  string time;
+  unsigned short room;
+  string to_string() const {
+    return "Lesson[Title: " + title + ", date: " + date + ", time: " + time
+        + ", room: " + std::to_string(room) + "]\n";
+  }
+} global_lesson_variable;
+
+//void PrintLesson(const string &title, const string &date, const string &time, unsigned short &room) {
+//  cout << "Title: " << title
+//       << ", date: " << date
+//       << ", time: " << time
+//       << ", room: " << room << '\n';
+//}
+//void PrintTimeTable(const vector<string> &titles, const vector<string> &dates, const vector<string> &times,
+//                    vector<unsigned short> &rooms) {
+//  int i = 0;
+//  while (i < titles.size()) {
+//    // НО м.б. проблемы, если данные не согласованы, например - размер векторов разный
+//    PrintLesson(titles[i], dates[i], times[i], rooms[i]);
+//    ++i;
+//  }
+//}
+void PrintLesson(const Lesson &lesson) {
+//  cout << "Title: " << lesson.title
+//       << ", date: " << lesson.date
+//       << ", time: " << lesson.time
+//       << ", room: " << lesson.room << '\n';
+  cout << lesson.to_string();
 }
-void PrintTimeTable(const vector<string> &titles, const vector<string> &dates, const vector<string> &times,
-                    vector<unsigned short> &rooms) {
-  int i = 0;
-  while (i < titles.size()) {
-    PrintClass(titles[i],
-               dates[i],
-               times[i],
-               rooms[i]); // НО м.б. проблемы, если данные не согласованы - размер векторв разный
-    ++i;
+void PrintTimeTable(const vector<Lesson> &lessons) {
+  for (auto &x: lessons) {
+    PrintLesson(x);
   }
 }
 
 int main() {
-  vector<string> titles = {"Programming", "Math Analysis", "Geometry"};
-  vector<string> dates = {"07.10.2019", "07.10.2019", "07.10.2019"};
-  vector<string> times = {"08:15", "09:45", "11:15"};
-  vector<unsigned short> rooms = {521, 513, 250};
-  PrintTimeTable(titles, dates, times, rooms);
+  // БЕЗ СТРУКТУР
+//  vector<string> titles = {"Programming", "Math Analysis", "Geometry"};
+//  vector<string> dates = {"07.10.2019", "07.10.2019", "07.10.2019"};
+//  vector<string> times = {"08:15", "09:45", "11:15"};
+//  vector<unsigned short> rooms = {521, 513, 250};
+//  PrintTimeTable(titles, dates, times, rooms);
+
+  // С ИСПОЛЬЗОВАНИЕМ СТРУКТУР
+  vector<Lesson> lessons = {{"Programming", "07.10.2019", "08:15", 521},
+                            {"Math Analysis", "07.10.2019", "09:45", 513},
+                            {"Geometry", "07.10.2019", "11:15", 250}};
+  PrintTimeTable(lessons);
+
+  global_lesson_variable = {"Programming", "07.10.2019", "08:15", 521};
+  cout << global_lesson_variable.to_string();
 
   return 0;
 }
