0715f13cb38d7b34a8d96e3a7fb9d8c9b880e189
avelana <o.g.kazantseva@gmail.com>
2019-10-09 23:33:26 +0300

    input\output in UTF16

--- 01-linear-algorithms/DemoLocal-ver2/main.cpp
+++ 01-linear-algorithms/DemoLocal-ver2/main.cpp
@@ -5,27 +5,21 @@
 #include <io.h>
 #include <fcntl.h>
 
-int main(int argc, wchar_t *argv[]) {
+int main() {
   _setmode(_fileno(stdout), _O_U16TEXT);
   _setmode(_fileno(stdin), _O_U16TEXT);
   _setmode(_fileno(stderr), _O_U16TEXT);
 
   std::wcout << L"Unicode -- English -- Русский -- Ελληνικά -- Español." << std::endl;
   // или
-  wprintf(L"%s", L"Unicode -- English -- Русский -- Ελληνικά -- Español.\n");
+  //wprintf(L"%s", L"Unicode -- English -- Русский -- Ελληνικά -- Español.\n");
 
   wchar_t ch;
   std::wcin >> ch;
 
   std::wcout << L"символ = " << ch << ".\n";
-
-//  auto  tmp = GetConsoleOutputCP();
-//  string test = u8"Russian:вапршывгарп Greek: αβγδ; German: Übergrößenträger";
-//  SetConsoleOutputCP(CP_UTF8);
-//  cout << test;
-
-//  SetConsoleOutputCP(tmp);
-
+  std::wstring str{ch};
+  std::wcout << L"символ в строку = " << str << '\n';
 
   return 0;
 }
